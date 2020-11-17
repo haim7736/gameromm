bb7d21a30c571eaea9b256cfe876236876a5e8e7
haim7736 <haim7736@naver.com>
2020-11-17 20:29:15 +0900

    1동

--- Assets/Scenes/Script/Cameramove.cs
+++ Assets/Scenes/Script/Cameramove.cs
@@ -7,19 +7,43 @@ public class Cameramove : MonoBehaviour
     public Camera cam;
     public Text a;
     public int check;
-    
+    public GameObject upbutton;
+    public GameObject Downbutton;
+    bool building;
+
     // Start is called before the first frame update
     void Start()
     {
         check = 0;
         a.text = "입원실";
-       
-        
+        upbutton.SetActive(false);
+        Downbutton.SetActive(false);
+        building = true;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        if(check == 1)
+        {
+            upbutton.SetActive(true);
+        }
+        else
+        {
+            upbutton.SetActive(false);
+        }
+
+        if (check == 8)
+        {
+            Downbutton.SetActive(true);
+        }
+        else
+        {
+            Downbutton.SetActive(false);
+        }
 
         switch (check)
         {
@@ -55,23 +79,68 @@ public class Cameramove : MonoBehaviour
                 cam.transform.position = new Vector3(65.58f, -12, -10);
                 a.text = "정문";
                 break;
+            case 8:
+                cam.transform.position = new Vector3(22.03f, 12, -10);
+                a.text = "1동 복도";
+                break;
+            case 9:
+                cam.transform.position = new Vector3(43.83f, 12, -10);
+                a.text = "1동 계단";
+                break;
+            case 10:
+                cam.transform.position = new Vector3(65.58f, 12, -10);
+                a.text = "1동 로비";
+                break;
         }
     }
 
     public void onLeft()
     {
-        check--;
-        if(check <= 0)
+        if (building)
         {
-            check = 0;
+            check--;
+            if (check <= 0)
+            {
+                check = 0;
+            }
+        }
+        else
+        {
+            check--;
+            if (check <= 8)
+            {
+                check = 8;
+            }
         }
     }
     public void onRight()
     {
-        check++;
-        if(check >=7)
+        if (building)
         {
-            check = 7;
+            check++;
+            if (check >= 7)
+            {
+                check = 7;
+            }
+        }else
+        {
+            check++;
+            if (check >= 10)
+            {
+                check = 10;
+            }
         }
+    }
+    public void onUP()
+    {
+        building = false;
+        check = 8;
+    }
+
+    public void onDown()
+    {
+        Debug.Log("C");
+        building = true;
+        check = 1;
     }
 }
